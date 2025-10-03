bd072518fc489e4f9b2d98735bb87cfe55dd1412
pavankumarkasula73 <pavankumarkasula73@gmail.com>
2025-10-03 19:08:46 +0530

    updated modules

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -1,7 +1,7 @@
 #my backend configuration that uses S3 for state storage and DynamoDB for state locking
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-3tier-pavan-20250929" 
+    bucket         = "my-terraform-state-3tier-pavan-20250930" 
     key            = "3tier/dev/terraform.tfstate"
     region         = "us-east-1"                               
     dynamodb_table = "terraform-lock-3tier-pavan"              
